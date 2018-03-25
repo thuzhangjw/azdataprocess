652d5befc3dc7d0023fdfb6cfc81284064c6d8e1
thuzhangjw <thuzhangjw@163.com>
2018-03-25 14:39:17 +0800

    word2vec

--- participle.py
+++ participle.py
@@ -2,12 +2,12 @@ import pandas as pd
 import jieba
 import sys
 import os 
-
+import re 
 
 df = pd.read_csv(sys.argv[1], sep ='\t')
 jieba.load_userdict('./my_dict_from_other_source.txt')
 jieba.load_userdict('./mydict.txt')
-
+rs = [re.compile('[(（][^(（]*[)）]'), re.compile('患者自发病以来.*。')]
 stopwords = set()
 
 with open('./stopword-full.dic', 'r') as f:
@@ -25,19 +25,51 @@ def load_suggest_freq():
              jieba.suggest_freq((words[0], words[1]), True)
         f.close()
 
+neg_words=['无']
+def add_negtive_word(wordslist):
+    preprocessd = list(map(lambda x: [x, '、'][x == '及'], wordslist))
+#    preprocessd = wordslist 
+    pos = []
+    for idx, word in enumerate(preprocessd):
+        for nw in neg_words:
+            if word == nw:
+                for i in range(idx+1, len(preprocessd)-1):
+                    if preprocessd[i] == '、' and preprocessd[i+1] not in neg_words:
+                        pos.append((i+1, nw))
+                    elif preprocessd[i] == ',' or preprocessd[i] == '，' or preprocessd[i] == '.' or preprocessd[i] == '。':
+                        break
+    res = preprocessd
+    for idx, p in enumerate(pos):
+        res.insert(idx + p[0], p[1])
+    return res 
+
+
 load_suggest_freq()
 sentence_list = []
+word_list_list = []
 for text in df['现病史']:
-    print(text, '\n')
-    seg_list = jieba.lcut(text)
-    s = ''
-    for word in seg_list:
-        if word not in stopwords:
-            s += word + ' ';
-    s = s.strip()
-    print(s, '\n')
-    sentence_list.append(s)
-    input()
+#    print(text, '\n')
+    for r in rs:
+        text = r.sub('', text)
+    sentences = text.split('。')
+    newdoc = ''
+    for s in sentences:
+        news = ''
+        if s == '':
+            continue
+        seg_list = jieba.lcut(s)
+        processd_list = add_negtive_word(seg_list)
+#        processd_list = seg_list 
+        for word in processd_list:
+            if word not in stopwords:
+                news += word + ' '
+        news += '。 '
+        word_list_list.append(news.strip('。 '))
+        newdoc += news 
+    newdoc = newdoc.strip()
+#    print(newdoc, '\n')
+    sentence_list.append(newdoc)
+#    input()
 
 #from collections import Counter
 #
@@ -52,4 +84,8 @@ newdf = pd.DataFrame({'disease_his' : sentence_list})
 newdf = pd.concat([newdf, df], axis=1)
 
 newdf.to_csv('../data/participled.txt', sep='\t', index=False)
+
+with open(sys.argv[2], 'w') as f:
+    for s in word_list_list:
+        f.write(s + '\n')
 
---
+++ word2vec.py
@@ -0,0 +1,43 @@
+import gensim
+import sys
+import copy
+
+sentences = gensim.models.word2vec.LineSentence('../data/corups.txt')
+#model = gensim.models.word2vec.Word2Vec(min_count=1, window=5, size=100, workers=16)
+#model.build_vocab(sentences)
+#
+#loss = sys.maxsize
+#pos = 0
+#
+#for i in range(10):
+#    model.train(sentences, total_examples=model.corpus_count, epochs=1, compute_loss=True)
+#    nloss = model.get_latest_training_loss()
+#    if nloss < loss:
+#        loss = nloss
+#        final_model = copy.deepcopy(model)
+#        pos = i + 1
+#
+#
+#print('loss:', loss, ' at iter: ', pos)
+#
+#final_model.save('../data/word2vec.model')
+#
+
+model = gensim.models.word2vec.Word2Vec(sentences, min_count=0, workers=16, iter=3, compute_loss=True, size=80)
+print('loss:', model.get_latest_training_loss())
+
+#model.save('../data/word2vec.model')
+
+
+loss = model.get_latest_training_loss()
+for i in range(7):
+    model.train(sentences, total_examples=model.corpus_count, compute_loss=True, epochs=model.epochs)
+    nloss = model.get_latest_training_loss()
+    if nloss < loss:
+        loss = nloss
+        final_model = copy.deepcopy(model)
+        print('nloss:', nloss)
+
+print('final loss:', loss)
+final_model.save('../data/word2vec.model')
+
